bc0b5690e5b9ffcb91a6f81160bda8645449fd2e
kong <kong.hou@nine.com.au>
2022-04-12 21:28:49 +1000

    wip: hello rustaceans

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use ferris_says::*;
+use std::io::{stdout, BufWriter};
+
+fn main(){
+    let stdout = stdout();
+    let message = String::from("hello fellow Rustaceans!");
+    let width = message.chars().count();
+
+    let mut writer = BufWriter::new(stdout.lock());
+    say(message.as_bytes(), width, &mut writer).unwrap();
 }
