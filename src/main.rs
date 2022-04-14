4ff95630acb586e69bdabfb738262b13a5a5e28f
kong <kong.hou@nine.com.au>
2022-04-14 20:41:26 +1000

    wip: programming a guess game

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-use ferris_says::*;
-use std::io::{stdout, BufWriter};
+use std::io;
 
 fn main(){
-    let stdout = stdout();
-    let message = String::from("hello fellow Rustaceans!");
-    let width = message.chars().count();
+    println!("Guess the number!");
+    println!("Please input your guess");
+    let mut guess = String::new();
+    
+    io::stdin().read_line(&mut guess).expect("Failed to read line");
 
-    let mut writer = BufWriter::new(stdout.lock());
-    say(message.as_bytes(), width, &mut writer).unwrap();
+    println!("You guessed: {}", guess);
 }
